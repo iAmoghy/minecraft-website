fc8529ab136cf4843b3e18375ba7da2e0d3ee453
iAmoghy <70843557+iAmoghy@users.noreply.github.com>
2021-10-21 07:34:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+$("caves-scroll").click(function() {
+    $("caves-target")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+});
+
+$("cliffs-scroll").click(function() {
+    $("cliffs-target")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+});
+
+$("misc-scroll").click(function() {
+    $("caves-target")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+});
+
+$("download-scroll").click(function() {
+    $("download-target")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+});
